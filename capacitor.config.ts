4df85a2a2a6cacb4e2c1b51e71433e593a74988c
Maxim Baryshov <maximbarishov06@gmail.com>
2026-07-15 14:07:08 +0300

    feat: add splash screen plugin configuration, assets, and custom build scripts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,22 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.maksimbarisov.flowfit',
-  appName: 'FlowFit',
-  webDir: 'www'
+  appName: 'Flow Fit',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1500,
+      launchAutoHide: true,
+      backgroundColor: '#F5F5F5',
+      showSpinner: false,
+      androidScaleType: 'CENTER_CROP',
+      splashFullScreen: true,
+      splashImmersive: true,
+      androidSplashResourceName: 'splash',
+      layoutName: 'launch_screen',
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
