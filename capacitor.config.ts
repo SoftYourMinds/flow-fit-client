bbde8310889f4f047263b55a7c22dd3537b62b62
Maxim Baryshov <maximbarishov06@gmail.com>
2026-07-15 13:27:14 +0300

    chore: rename app, update capacitor dependencies, and configure npm registry settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'temp_ionic',
+  appId: 'com.maksimbarisov.flowfit',
+  appName: 'FlowFit',
   webDir: 'www'
 };
 
