e12d799ccb5a45668d5485afb6afafa077f0b211
Maxim Baryshov <maximbarishov06@gmail.com>
2026-08-22 23:06:40 +0300

    feat(pwa): add Web App Manifest, ServiceWorker, and icon suite for installable PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,17 +14,19 @@ import { authInterceptor } from './core/auth/auth.interceptor';
 import { globalLoaderInterceptor } from './core/interceptors/global-loader.interceptor';
 import { globalErrorInterceptor } from './core/interceptors/global-error.interceptor';
 
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
-import { QuillModule } from 'ngx-quill';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, QuillModule.forRoot()],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideHttpClient(withInterceptors([globalLoaderInterceptor, authInterceptor, globalErrorInterceptor])),
-    provideCharts(withDefaultRegisterables())
+    provideCharts(withDefaultRegisterables()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
